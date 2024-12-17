9b78c7122f2a469b5980b51a018e33a502b83386
litengfei <1164834201@qq.com>
2024-12-17 22:50:12 +0800

    associate value

--- Swift_Practice.playground/Pages/04-Enumeration.xcplaygroundpage/Contents.swift
+++ Swift_Practice.playground/Pages/04-Enumeration.xcplaygroundpage/Contents.swift
@@ -2,22 +2,27 @@
 
 import Foundation
 
+//什么是枚举，规定类型，
 enum Direction {
     case north
     case west
 }
 
 var dirct = Direction.north
+dirct = .west
 print (dirct)
 
 
 //Associate values
 enum Game {
     case team(String)
+    case date(Year:Int,Month:Int,Day:Int)
     case score(Int)
 }
 
 var a = Game.team("马刺")
+var b = Game.date(Year: 2024, Month: 1, Day: 28)
+print(b)
 
 switch a{
 case .team("马刺"):
@@ -27,3 +32,29 @@ default:
 }
 
 
+enum Response {
+    case success(Message:String)
+    case error(code:Int,description:String)
+}
+
+var result1 = Response.success(Message: "hello world")
+var result2 = Response.error(code: 404, description: "未找到网页")
+//
+//swtch result1{
+//case let  .success(Message: i):
+//    print(i)
+////case let .error(let code, let description):
+////    print("\(code) : \(description)")
+//default:
+//    break
+//}
+
+//
+switch result1 {
+case let .success(M: i):
+    print(i)
+case let .error(code: a, description: b):
+    print("\(a) : \(b)")
+default:
+    break
+}
