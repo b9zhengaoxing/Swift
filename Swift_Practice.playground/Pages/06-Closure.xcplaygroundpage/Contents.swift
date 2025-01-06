a1e4ee008f0036899e6d2f29544da45ae837204e
litengfei <1164834201@qq.com>
2025-01-06 22:40:43 +0800

    udpate closuer
    1. 简化   类型 return  缩写 完全简化

--- Swift_Practice.playground/Pages/06-Closure.xcplaygroundpage/Contents.swift
+++ Swift_Practice.playground/Pages/06-Closure.xcplaygroundpage/Contents.swift
@@ -2,6 +2,7 @@
 
 import Foundation
 
+//一： 标准闭包
 //Clousure Expression 定义一个函数
 func sum(_ v1:Int, _ v2 :Int) -> Int{
     v1 + v2
@@ -21,33 +22,29 @@ print(
     }(10,20)
 )
 
-//闭包的缩写
+//二 闭包的缩写
 func exec(v1:Int , v2:Int , fnc:(Int,Int)->Int){
     print(fnc(v1 , v2))
 }
 
-//完整的闭包
+//0. 完整的闭包
 exec(v1: 10, v2: 20, fnc: {
     (a1:Int , a2:Int) in
     return a1 + a2
 })
 
-//类型判断
-exec(v1: 11, v2: 12, fnc: {
-    (a1, a2) in
-    a1 + a2
-})
-
+//1. 参数 类型 返回值 类型  1.1 一个表达式，return
 exec(v1: 1, v2: 2, fnc: {
     a1, a2 in
     a1 + a2
 })
 
+//2. 参数名缩写
 exec(v1: 22, v2: 17, fnc: {
     $0 + $1
 })
 
-//极致简化了
+//3. 完全Omit
 exec(v1: 2, v2: 2, fnc: - )
 
 
