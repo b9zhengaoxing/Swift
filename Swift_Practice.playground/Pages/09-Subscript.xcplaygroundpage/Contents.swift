b4dbae9ec567fb6ff6b72f576affb665731e698f
litengfei <1164834201@qq.com>
2025-01-21 22:19:37 +0800

    Subscript
    提供下边访问，本质还是方法

--- Swift_Practice.playground/Pages/09-Subscript.xcplaygroundpage/Contents.swift
+++ Swift_Practice.playground/Pages/09-Subscript.xcplaygroundpage/Contents.swift
@@ -2,14 +2,15 @@
 
 import Foundation
 
-//Subscripts Access the elements of a collection
+//Subscripts Access the elements of a collection  p[] subcript
+//本质是方法
 
 class Piont{
     var x = 0.0 , y = 0.0
     subscript (index:Int) -> Double{
         set{
             print("hello \(newValue)")
-            if newValue == 0{
+            if index == 0{
                 print("hello 0 \(newValue)")
                 x = newValue
             }else{
@@ -32,3 +33,41 @@ print("\(point.x) \(point.y)")
 point[0] = 10
 point[1] = 12
 print("\(point[0]) \(point[1])")
+
+
+//Subscript 必须有set ,只有set omit set
+//多个参数，任意类型，可以设置 参数标签，可以是类方法
+
+class Sum{
+    static subscript(V1 i:Int,V2 b:Int) -> Int{
+        i + b
+    }
+}
+
+print(Sum[V1:1,V2:2])
+
+
+class Grid{
+    var data = [[0,1,2],[3,4,5],[6,7,8]]
+    subscript(row:Int,column:Int)->Int{
+        set{
+            if row < 0 && row >= 3 && column < 0 && column >= 3{
+                return
+            }
+            else{
+                data[row]column] = newValue
+            }
+                
+        }get{
+            if row < 0 && row >= 3 && column < 0 && column >= 3{
+                return 0
+            }else{
+                return data[row,column]
+            }
+        }
+    }
+}
+
+var grid = Grid()
+grid[0,0] = 10
+print(grid[0,0])
