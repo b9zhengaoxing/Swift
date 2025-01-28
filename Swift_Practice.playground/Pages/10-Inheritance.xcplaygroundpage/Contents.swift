84c8350a9f4c93675daafc19190b7c0902518fd7
litengfei <1164834201@qq.com>
2025-01-28 22:58:38 +0800

    override 方法
    1. 实例方法，下标，可以重写
    2. 类方法 class 允许重写，static 不允许重写 ，不允许继承

--- Swift_Practice.playground/Pages/10-Inheritance.xcplaygroundpage/Contents.swift
+++ Swift_Practice.playground/Pages/10-Inheritance.xcplaygroundpage/Contents.swift
@@ -2,23 +2,33 @@
 
 import Foundation
 
-//值类型 不持之 Inheritance
+//值类型 不支持 Inheritance
 //继承 属性 方法
 
 class animal{
     var age = 0
-    func type() -> String {
-        return "animal"
+    func type() { // 重写实例方法
+        print("animal")
+    }
+    
+    subscript(Index:Int) -> Int{
+        return Index
     }
 }
 
 class Dog:animal{
     var height = 0
     
-    override func type() -> String {
-        return "Dog"
+    override func type() {
+        super.type()
+        print("Dog")
+    }
+    
+    override subscript(Index: Int) -> Int {
+        return super[Index] + 1
     }
 }
 
 var dog1 = Dog()
-print("\(dog1.age)")
+dog1.type()
+print(dog1[1])
