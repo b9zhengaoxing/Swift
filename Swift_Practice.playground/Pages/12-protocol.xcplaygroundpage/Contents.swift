ea0e5475a7c34424e7e3d7b986cbb798b3ec8261
litengfei <1164834201@qq.com>
2025-02-06 22:51:02 +0800

    update
    Signed-off-by: litengfei <litengfei@qiyi.com>

--- Swift_Practice.playground/Pages/12-protocol.xcplaygroundpage/Contents.swift
+++ Swift_Practice.playground/Pages/12-protocol.xcplaygroundpage/Contents.swift
@@ -4,15 +4,26 @@ import Foundation
 
 protocol PersonRule{
     var name:String{set get}////initial value is not allowed here
+    var age:Int{set get}
     subscript(index:Int)->Int {set get}//subscript in protocol must have explicit { get } or { get set } specifier
     func eat(food:String)
 }
 
 class Chinese:PersonRule{
     var name:String = "中国人"
+    private var _age:Int = 0
+    var age:Int{
+        set{
+            _age = newValue
+        }
+        get{
+            return _age
+        }
+    }
     var index:Int = 1
-    init(name: String) {
+    init(name: String,age:Int) {
         self.name = name
+        self.age = age
     }
     func eat(food:String) {print("Today eat \(food)")}
     
