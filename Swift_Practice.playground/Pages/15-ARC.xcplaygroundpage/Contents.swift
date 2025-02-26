b6570c6877997ac1bb2913670b7c9e0a56758fc2
litengfei <1164834201@qq.com>
2025-02-26 22:50:57 +0800

    相互之间强引用

---
+++ Swift_Practice.playground/Pages/15-ARC.xcplaygroundpage/Contents.swift
@@ -0,0 +1,50 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//强引用
+class Person {
+    var fn:(()->())?
+    
+    func run() {
+        print("run")
+    }
+    deinit {
+        print("deinit")
+    }
+}
+
+func test(){
+    var person:Person = Person()
+    person.fn = {//循环引用风险
+        [weak person] in
+        person?.run()  //可选链条
+    }
+}
+test()
+
+//强引用
+class Dog<T>{
+    var hate:T?
+    deinit {
+        print("dog deinit")
+    }
+}
+
+class Cat{
+    var hate:AnyObject?
+    deinit {
+        print("cat deinit")
+    }
+}
+
+var cat:Cat? = Cat()
+var dog:Dog? = Dog<Cat>()
+cat?.hate = dog
+dog?.hate = cat
+cat = nil
+dog = nil
+
+
+
+
