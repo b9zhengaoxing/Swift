154789f04ebc8bcbf39ccfdc216dcd9e6a2bdef1
litengfei <1164834201@qq.com>
2025-03-01 13:18:32 +0800

    LeetCode TwoNumbers 暴力解法  for 越界崩溃
    1.  暴力突破
    2. 先排除越界情况
    3. count 和 range 越界问题

--- Swift_Practice.playground/Pages/21- leetcode.xcplaygroundpage/Contents.swift
+++ Swift_Practice.playground/Pages/21- leetcode.xcplaygroundpage/Contents.swift
@@ -1,21 +1,51 @@
 //: [Previous](@previous)
 
 import Foundation
-
-class TreeNode {
-    var val: Int
-    var left: TreeNode?
-    var right: TreeNode?
-    init(_ val: Int) {
-        self.val = val
-        self.left = nil
-        self.right = nil
+// Two numbers
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    var ret:[Int] = [Int]()
+    if nums.isEmpty{
+        return ret
     }
+    
+    for i in 0...nums.count{
+        for j in 0...nums.count{
+            if i == j{
+                print("i \(i) j \(j)")
+                continue
+            }
+            if target == nums[i] + nums[j]{
+                print("i \(i) j \(j)")
+                ret.append(i)
+                ret.append(j)
+                return ret
+            }
+        }
+    }
+    return ret
 }
 
-class Solution {
 
+//暴力解法
+//1. 暴力突破
+//2. 先排除越界情况
+//3. count 和 range 越界问题
+func twoSum1(_ nums: [Int], _ target: Int) -> [Int] {
+    var ret:[Int] = [Int]()
+    if nums.isEmpty || nums.count < 2{//为空或者小于2 退出
+        return ret
+    }
+    
+    for i in 0..<nums.count{
+        for j in i+1..<nums.count{
+            if target == nums[i] + nums[j]{
+                print("i \(i) j \(j)")
+                ret.append(i)
+                ret.append(j)
+                return ret
+            }
+        }
+    }
+    return ret
 }
 
-var solution = Solution()
-solution.binaryTreePaths([1,2,3,null,5])
