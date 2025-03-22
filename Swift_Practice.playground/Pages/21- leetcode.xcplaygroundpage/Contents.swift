4f132d1e58e02748ea89976b51cbce42c05fdc78
litengfei <1164834201@qq.com>
2025-03-22 15:00:05 +0800

    0 1 背包问题解决
    二维数组 VS 一维数组

--- Swift_Practice.playground/Pages/21- leetcode.xcplaygroundpage/Contents.swift
+++ Swift_Practice.playground/Pages/21- leetcode.xcplaygroundpage/Contents.swift
@@ -2,26 +2,68 @@
 
 import Foundation
 
-func eraseOverlapIntervals(_ intervals: [[Int]]) -> Int {
-    //count 1
-    if intervals.count == 1 {
-        return 0
+//背包问题 二维
+
+var firstLine = [6,8]
+var spaces = [2,2,3,1,5,2]
+var values = [2,3,1,5,4,3]
+
+
+
+func oneOpackage(_ firstLine:[Int],_ spaces:[Int], _ values:[Int]){
+    let m = firstLine[0]
+    let n = firstLine[1]
+
+    // 初始化二维数组，dp[i][j]表示前i个物品在容量j时的最大价值
+    var dp = [[Int]](repeating: [Int](repeating: 0, count: n + 1), count: m + 1)
+    
+    for j in spaces[0] ... n {
+        dp[0][j] = values[0]
     }
-
-    //贪心算法 找最大公共区间数
-    var sortedIntervals = intervals.sorted{$0[0] < $1[0] || ($0[0] == $1[0] && $0[1] < $1[1])}
-    var res = 0
-
-    //大于1
-    for i in 1 ..< sortedIntervals.count{
-        if sortedIntervals[i - 1][1] > sortedIntervals[i][0]{
-            res += 1
-            sortedIntervals[i][1] = min(sortedIntervals[i - 1][1],sortedIntervals[i][1])
+    
+    for i in 0 ..< m {
+        let space = spaces[i]
+        let value = values[i]
+        for j in 0...n {
+            if j < space {
+                // 容量不足，无法选择当前物品
+                dp[i+1][j] = dp[i][j]
+            } else {
+                // 可以选择或不选择当前物品，取较大值
+                //
+                dp[i+1][j] = max(dp[i][j]/*不放物品i */, dp[i][j - space] + value /*放物品 i */)
+            }
         }
     }
-    return res
+    print(dp[m][n])
 }
 
-var test = [[1,2],[2,3],[3,4],[1,3]]
-print(eraseOverlapIntervals(test))
+
+func oneOpackage1(_ firstLine:[Int],_ spaces:[Int], _ values:[Int]){
+
+    let m = firstLine[0]
+    let n = firstLine[1]
+    var dp = [Int](repeating: 0, count: n + 1)//初始化为1
+    for i in 0 ..< m {//先递归价值
+        let space = spaces[i]
+        let value = values[i]
+        for j in (space...n).reversed() {//再递归空间
+            if j >= space {
+                dp[j] = max(dp[j], dp[j - space] + value)
+            }
+        }
+    }
+
+    print(dp[n])
+}
+
+
+
+
+oneOpackage(firstLine, spaces, values)
+oneOpackage1(firstLine, spaces, values)
+
+
+
+//背包问题 一维数组
 
